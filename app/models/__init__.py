34ad057f1e69d90cf6698a2e3848fe1a2c866394
Arthur-Santos13 <ahsantos1301@gmail.com>
2026-04-28 00:00:47 -0300

    feat: add domain models with auditing fields and category support

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.item import Item, ItemCategory
+from app.models.user_event import UserEvent, EventType
+
+__all__ = ["User", "Item", "ItemCategory", "UserEvent", "EventType"]
